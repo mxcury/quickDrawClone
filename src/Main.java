9f061506ddc88316a8880d30380afda49dcf71eb
Tom Flattery <flatterythomas48@gmail.com>
2022-08-19 23:39:41 +0100

    initial commit with testing global .gitignore

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+//this project will be a clone of Google's "Quick, Draw!" game using a CNN
